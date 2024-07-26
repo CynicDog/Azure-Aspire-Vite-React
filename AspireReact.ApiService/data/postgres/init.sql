a91da72b71317131d9c16b96b70ca34d3b4fb772
이은상 <kyrsang0801@inbrein.com>
2024-07-26 16:59:26 +0900

    Chore: added database init script

---
+++ AspireReact.ApiService/data/postgres/init.sql
@@ -0,0 +1,17 @@
+﻿-- Create the table
+CREATE TABLE WeatherSummaries (
+    Id SERIAL PRIMARY KEY,
+    Summary VARCHAR(50) NOT NULL
+);
+
+-- Insert the initial summaries
+INSERT INTO WeatherSummaries (Summary) VALUES ('Freezing');
+INSERT INTO WeatherSummaries (Summary) VALUES ('Bracing');
+INSERT INTO WeatherSummaries (Summary) VALUES ('Chilly');
+INSERT INTO WeatherSummaries (Summary) VALUES ('Cool');
+INSERT INTO WeatherSummaries (Summary) VALUES ('Mild');
+INSERT INTO WeatherSummaries (Summary) VALUES ('Warm');
+INSERT INTO WeatherSummaries (Summary) VALUES ('Balmy');
+INSERT INTO WeatherSummaries (Summary) VALUES ('Hot');
+INSERT INTO WeatherSummaries (Summary) VALUES ('Sweltering');
+INSERT INTO WeatherSummaries (Summary) VALUES ('Scorching');
