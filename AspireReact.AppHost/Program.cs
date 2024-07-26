bf81f9adc8031bc4c1cab2c565f90719f635d4de
이은상 <kyrsang0801@inbrein.com>
2024-07-26 16:58:37 +0900

    Feat: added persistence container

--- AspireReact.AppHost/Program.cs
+++ AspireReact.AppHost/Program.cs
@@ -2,12 +2,24 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
 
-var apiService = builder.AddProject<Projects.AspireReact_ApiService>("apiservice");
+var db_username = builder.AddParameter("psql-username", secret: true); 
+var db_password = builder.AddParameter("psql-password", secret: true);
+var db_name = "aspiringreact";
+var weatherDatabase = builder.AddPostgres(
+        "postgres",  
+        userName: db_username, 
+        password: db_password)
+    .WithEnvironment("POSTGRES_DB", db_name)
+    .WithBindMount(
+        "../AspireReact.ApiService/data/postgres",
+        "/docker-entrypoint-initdb.d")
+    .AddDatabase(db_name);
 
-// builder.AddProject<Projects.AspireReact_Web>("webfrontend")
-//     .WithExternalHttpEndpoints()
-//     .WithReference(cache)
-//     .WithReference(apiService);
+// To get inside the container, run: docker exec -it {PID} psql -U AspireIsAwesome -d aspiringreact 
+
+var apiService = builder
+    .AddProject<Projects.AspireReact_ApiService>("apiservice")
+    .WithReference(weatherDatabase);
 
 builder.AddNpmApp("react", "../aspiring-react")
     .WithExternalHttpEndpoints()
